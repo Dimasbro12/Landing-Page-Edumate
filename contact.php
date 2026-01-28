d0f81b130a5650b40c17947304523db467569e40
Dimasbro12 <dimas.23329@mhs.unesa.ac.id>
2026-01-28 15:00:30 +0700

    merapikan kode agar modular dan mencoba fitu hubungi kami

--- contact.php
+++ contact.php
@@ -1,6 +1,7 @@
 <?php
 // Menggunakan autoloader dari Composer (sesuaikan path jika folder vendor ada di luar folder Src)
-require 'vendor/autoload.php';
+require __DIR__ . '/vendor/autoload.php';
+
 
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
@@ -11,16 +12,16 @@ $mail->IsSMTP();
 
 $mail->SMTPSecure = 'ssl';
 
-$mail->Host = "localhost"; //hostname masing-masing provider email
+$mail->Host = "smtp.gmail.com"; 
 $mail->SMTPDebug = 2;
 $mail->Port = 465;
 $mail->SMTPAuth = true;
 
-$mail->Timeout = 60; // timeout pengiriman (dalam detik)
+$mail->Timeout = 60;
 $mail->SMTPKeepAlive = true; 
 
-$mail->Username = "admin@namadomain"; //user email
-$mail->Password = "XXXXX"; //password email
+$mail->Username = "dwid35895@gmail.com"; 
+$mail->Password = getenv('APP_PASSWORD'); 
 
 // Menangkap data dari form HTML
 $name = $_POST['name'] ?? 'No Name';
@@ -28,18 +29,27 @@ $email = $_POST['email'] ?? 'no-reply@example.com';
 $subject = $_POST['subject'] ?? 'No Subject';
 $message = $_POST['message'] ?? 'No Message';
 
-$mail->SetFrom("admin@namadomain", "Website Edumate"); // Email pengirim (harus sesuai autentikasi SMTP)
+$mail->SetFrom("dwid35895@gmail.com", "Website Edumate"); // Email pengirim (harus sesuai autentikasi SMTP)
 $mail->AddReplyTo($email, $name); // Agar ketika di-reply langsung ke email pengisi form
 $mail->Subject = "Pesan Baru: " . $subject;
-$mail->AddAddress("admin@namadomain", "Admin"); // Email tujuan (Admin)
+$mail->AddAddress("dwid35895@gmail.com", "Admin"); // Email tujuan (Admin)
 
 // Isi pesan email
 $mail->MsgHTML("<h3>Pesan Baru dari Website</h3><p><b>Nama:</b> $name</p><p><b>Email:</b> $email</p><p><b>Pesan:</b><br>$message</p>");
 
 try {
     $mail->Send();
-    echo "Message has been sent";
+    echo "<script>
+        alert('Pesan berhasil dikirim');
+        window.location.href = 'index.html';
+    </script>";
+    exit;
+    exit;
 } catch (Exception $e) {
-    echo "Failed to sending message. Mailer Error: {$mail->ErrorInfo}";
+    echo "<script>
+        alert('Gagal mengirim pesan');
+        window.location.href = 'index.html';
+    </script>";
+    exit;
 }
 ?>
